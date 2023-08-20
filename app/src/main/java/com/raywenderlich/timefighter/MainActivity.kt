7069b38b76f6ac6cf62f859e4203956415b5e0c0
MalachiV12303 <malachivalle12303@gmail.com>
2023-08-20 19:57:11 -0400

    Finished chapter 1 Assignments

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             TimeFighterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Malachi Valle from COP2660")
                 }
             }
         }
